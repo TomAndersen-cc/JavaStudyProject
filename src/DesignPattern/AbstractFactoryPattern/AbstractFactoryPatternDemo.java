b6ae96f11220f66e11a1d0cc62e2af0aa0e12652
TomAndersen-cc <1040994898@qq.com>
2020-01-14 19:24:05 +0800

    Java Study

--- src/DesignPattern/AbstractFactoryPattern/AbstractFactoryPatternDemo.java
+++ src/DesignPattern/AbstractFactoryPattern/AbstractFactoryPatternDemo.java
@@ -8,16 +8,16 @@ package DesignPattern.AbstractFactoryPattern;
  */
 public class AbstractFactoryPatternDemo {
     public static void main(String[] args) {
-        // 获取Shape工厂
-        AbstractFactory colorFactory = FactoryBuilder.getFactory("color");
+        // 获取Color工厂
+        AbstractFactory colorFactory = new ColorFactory();
         Color color1 = colorFactory.getColor("red");
         color1.fill();
         Color color2 = colorFactory.getColor("blue");
         color2.fill();
         Color color3 = colorFactory.getColor("green");
         color3.fill();
-        // 获取Color工厂
-        AbstractFactory shapeFactory = FactoryBuilder.getFactory("shape");
+        // 获取Shape工厂
+        AbstractFactory shapeFactory = new ShapeFactory();
         Shape shape1 = shapeFactory.getShape("circle");
         shape1.draw();
         Shape shape2 = shapeFactory.getShape("square");
